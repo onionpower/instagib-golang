c408bf77a92b4cb1c38d5a22800cc1f2ab2130be
onionpower <chocoonion@gmail.com>
2018-12-29 12:20:51 +0300

    palindrome wip

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import "fmt"
 
+func permute(nums []int) [][]int {
+	if len(nums) == 1 {
+		return append(make([][]int, 0), nums)
+	}
+	lv := nums[0]
+	r := permute(nums[1:])
+	l := []int{lv}
+	l = append(l, r[0]...)
+	r = append(r)
+	r[0] = append(r[0], lv)
+	r = append(r, l)
+	return r
+}
+
 func main() {
-	nums := []int{1, 2}
-	pivot := int(len(nums) / 2)
-	l := nums[0:pivot]
-	m := nums[pivot]
-	r := nums[pivot+1:]
-	fmt.Printf("%v %v %v", l, m, r)
+	nums := []int{1, 2, 3, 4}
+	p := permute(nums)
+
+	fmt.Printf("%v", p)
 }
