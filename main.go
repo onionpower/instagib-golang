f86ae42a5468930f14c2370ccc8e125327107b08
onionpower <chocoonion@gmail.com>
2018-12-29 11:52:24 +0300

    factorial

--- main.go
+++ main.go
@@ -1,39 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
-type A struct {
-	Value string
-}
-
-func write(wg *sync.WaitGroup, val string, ch chan<- A) {
-	defer wg.Done()
-	fmt.Println(val, "will be written")
-	ch <- A{Value: val}
-	fmt.Println(val, "was written")
-	fmt.Println(len(ch))
-}
-
-func read(ch <-chan A) {
-	fmt.Println(len(ch))
-	for v := range ch {
-		fmt.Println(v)
+func factorial(n int) int {
+	if n == 1 {
+		return 1
 	}
+	return n * factorial(n-1)
 }
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(3)
-	deps := make(chan A, 3)
-	defer close(deps)
-	go write(&wg, "A", deps)
-	go write(&wg, "B", deps)
-	go write(&wg, "C", deps)
-	wg.Wait()
-	go read(deps)
-	fmt.Println("done")
-	fmt.Scanln()
+	fmt.Println(factorial(4))
 }
