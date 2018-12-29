6e40d17438219a458099e993eab70a1beb6ab300
onionpower <chocoonion@gmail.com>
2018-12-29 12:12:08 +0300

    palindrome wip

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
-func reverse(l []int) []int {
-	if len(l) == 1 {
-		return l
-	}
-	return append(reverse(l[1:]), l[0])
-}
-
 func main() {
-	fmt.Printf("%v", reverse([]int{1, 2, 3, 4}))
+	nums := []int{1, 2}
+	pivot := int(len(nums) / 2)
+	l := nums[0:pivot]
+	m := nums[pivot]
+	r := nums[pivot+1:]
+	fmt.Printf("%v %v %v", l, m, r)
 }
