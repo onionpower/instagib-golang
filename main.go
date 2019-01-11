700c7b36ab388ea06498fd963426ed344a983ed4
onionpower <chocoonion@gmail.com>
2019-01-11 18:55:17 +0300

    roman to int: amended

--- main.go
+++ main.go
@@ -2,13 +2,39 @@ package main
 
 import "fmt"
 
-func reverse(l []int) []int {
-	if len(l) == 1 {
-		return l
+func romanToInt(s string) int {
+	w := map[uint8]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
 	}
-	return append(reverse(l[1:]), l[0])
+
+	if s == "" {
+		return 0
+	}
+	r := 0
+	greatest := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		cw := w[s[i]]
+		if cw > greatest {
+			r += cw
+			greatest = cw
+		} else if cw < greatest {
+			r -= cw
+		} else {
+			r += cw
+		}
+	}
+	return r
 }
 
 func main() {
-	fmt.Printf("%v", reverse([]int{1, 2, 3, 4}))
+	fmt.Println(romanToInt("IIIV"))
+	fmt.Println(romanToInt("IX"))
+	fmt.Println(romanToInt("LVIII"))
+	fmt.Println(romanToInt("IIIL"))
 }
