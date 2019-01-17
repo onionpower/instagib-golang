b2208f22a0e442cf24841a756bff4f9faf0fbfb6
onionpower <chocoonion@gmail.com>
2019-01-17 16:12:27 +0300

    remove duplicates: recursive

--- main.go
+++ main.go
@@ -1,63 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
+func removeDuplicates(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+	return removeDuplicatesInternal(&nums, 0, 1)
 }
 
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil {
-		return l2
+func removeDuplicatesInternal(p_nums *[]int, it int, next int) int {
+	nums := *p_nums
+	if next >= len(nums) {
+		return 1
 	}
-	if l2 == nil {
-		return l1
+	if nums[it] == nums[next] {
+		return removeDuplicatesInternal(p_nums, it, next+1)
 	}
-	if l1.Val < l2.Val {
-		l1.Next = mergeTwoLists(l1.Next, l2)
-		return l1
-	}
-	l2.Next = mergeTwoLists(l1, l2.Next)
-	return l2
-}
-
-func convToLl(ints []int) *ListNode {
-	if ints == nil || len(ints) == 0 {
-		return nil
-	}
-	first := &ListNode{ints[0], nil}
-	it := first
-	for i := 1; i < len(ints); i++ {
-		it.Next = &ListNode{ints[i], nil}
-		it = it.Next
-	}
-	return first
-}
-
-func llJoin(ll *ListNode) string {
-	if ll == nil {
-		return ""
-	}
-
-	str := strings.Builder{}
-	for node := ll; node != nil; node = node.Next {
-		str.WriteString(strconv.Itoa(node.Val))
-	}
-	return str.String()
+	it++
+	nums[it] = nums[next]
+	return 1 + removeDuplicatesInternal(p_nums, it, next+1)
 }
 
 func main() {
-	l0 := convToLl([]int{1, 2, 3})
-	fmt.Println(llJoin(l0))
-	l1 := convToLl([]int{2, 4, 5})
-	fmt.Println(llJoin(l1))
-	res := mergeTwoLists(l0, l1)
-	fmt.Println(llJoin(res))
-	fmt.Println(llJoin(mergeTwoLists(nil, nil)))
-	fmt.Println(llJoin(mergeTwoLists(nil, convToLl([]int{1, 2}))))
+	fmt.Println(removeDuplicates([]int{1, 1, 2}))
+	fmt.Println(removeDuplicates([]int{1, 2, 3}))
+	fmt.Println(removeDuplicates([]int{1, 2, 2, 3}))
+	fmt.Println(removeDuplicates([]int{1, 2, 3, 4}))
+	fmt.Println(removeDuplicates([]int{2, 2, 3, 4}))
+	fmt.Println(removeDuplicates([]int{2, 2, 3, 3}))
+	fmt.Println(removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
 }
