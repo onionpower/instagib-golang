e6cf1716e61fe0d2dad05d7582be6bd18eba3087
onionpower <chocoonion@gmail.com>
2019-01-22 15:42:15 +0300

    search insert position

--- main.go
+++ main.go
@@ -4,37 +4,33 @@ import (
 	"fmt"
 )
 
-func removeElement(nums []int, val int) int {
-	if nums == nil {
+func searchInsert(nums []int, target int) int {
+	if len(nums) == 1 {
+		if nums[0] < target {
+			return 1
+		}
 		return 0
 	}
-
-	start, end := 0, len(nums)-1
-	for start <= end {
-		if nums[start] == val {
-			nums[end], nums[start] = nums[start], nums[end]
-			end--
-		} else {
-			start++
-		}
+	pivot := len(nums) / 2
+	pv := nums[pivot]
+	if pv == target {
+		return pivot
 	}
-	return start
+	if pv > target {
+		return searchInsert(nums[:pivot], target)
+	}
+	return pivot + searchInsert(nums[pivot:], target)
 }
 
 func main() {
-	printRemoved([]int{0, 1, 2, 2, 3, 0, 4, 2}[:], 2)
-	printRemoved([]int{1, 3, 3, 4}[:], 3)
-	printRemoved([]int{1, 3, 3, 4}[:], 5)
-	printRemoved([]int{1, 3, 3, 4}[:], 0)
-	printRemoved([]int{3, 3}[:], 3)
-	printRemoved([]int{3}[:], 3)
+	printSln([]int{1, 3, 5, 6}[:], 5)
+	printSln([]int{1, 3, 5, 6}[:], 1)
+	printSln([]int{1, 3, 5, 6}[:], 7)
+	printSln([]int{1, 3, 5, 6}[:], 0)
+	printSln([]int{1, 3, 5, 6}[:], 2) // TODO wrong
 }
 
-func printRemoved(nums []int, val int) {
-	fmt.Printf("%v\n%v", nums, val)
-	fmt.Println()
-	length := removeElement(nums[:], val)
-	fmt.Printf("%v", nums[0:length])
-	fmt.Println()
-	fmt.Println()
+func printSln(nums []int, target int) {
+	fmt.Printf("%v, target: %v\n", nums, target)
+	fmt.Printf("supposed pos is %v\n", searchInsert(nums, target))
 }
