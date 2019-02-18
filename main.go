9a8d23fdebe2a38d31ff4087761de1dccccc06c6
onionpower <chocoonion@gmail.com>
2019-02-19 01:11:58 +0300

    sqrt(x)

--- main.go
+++ main.go
@@ -1,32 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func plusOne(digits []int) []int {
-	if digits == nil {
-		return []int{1}
+func mySqrt(x int) int {
+	s := uint64(x)
+	uix := uint64(x)
+	for s*s > uix {
+		s = (s + uix/s) / 2
 	}
-
-	return plusOneInternal(digits)
-}
-
-func plusOneInternal(digits []int) []int {
-	o := 1
-	for i := len(digits) - 1; i >= 0; i-- {
-		t := digits[i] + o
-		digits[i] = t % 10
-		o = int(t / 10)
-	}
-
-	if o > 0 {
-		return append([]int{1}, digits...)
-	}
-
-	return digits
+	return int(s)
 }
 
 func main() {
-	fmt.Println(plusOne([]int{1, 2, 0}))
-	fmt.Println(plusOne([]int{1, 0, 1}))
-	fmt.Println(plusOne([]int{9, 9}))
+	fmt.Println(mySqrt(8))
+	fmt.Println(mySqrt(10))
 }
