7d9d81d1ff9edf0f32c26b12fb2b5ec2a76f6fad
onionpower <chocoonion@gmail.com>
2019-02-26 21:19:57 +0300

    hackerrank: breaking-best-and-worst-records

--- main.go
+++ main.go
@@ -1,34 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	_ "github.com/segmentio/kafka-go/lz4"
+)
 
-// x1 + v1 * y = x2 + v2 * y
-// x1 + v1 * y - v2 * y = x2
-// y * (v1 - v2) = x2 - x1
-// y = (x2 - x1)/(v1 - v2)
-func kangaroo(x1, v1, x2, v2 int32) string {
-	if x1 < 0 || x2 < x1 || x2 > 10000 {
-		return "NO"
+func breakingRecords(scores []int32) []int32 {
+	if scores == nil || len(scores) == 0 {
+		return []int32{0, 0}
 	}
-	if v1 < 1 || v1 > 10000 {
-		return "NO"
+	min, max := scores[0], scores[0]
+	var minScores, maxScores int32 = 0, 0
+	scoresLen := len(scores)
+	for i := 1; i < scoresLen; i++ {
+		it := scores[i]
+		if it < min {
+			min = it
+			minScores++
+			continue
+		}
+		if it > max {
+			max = it
+			maxScores++
+		}
 	}
-	if v2 < 1 || v2 > 10000 {
-		return "NO"
-	}
-	return kangarooInternal(float64(x1), float64(v1), float64(x2), float64(v2))
-}
-
-func kangarooInternal(x1, v1, x2, v2 float64) string {
-	steps := (x2 - x1) / (v1 - v2)
-	if steps > 0 && steps <= 10000 && steps == float64(int32(steps)) {
-		return "YES"
-	}
-	return "NO"
+	return []int32{maxScores, minScores}
 }
 
 func main() {
-	fmt.Println(kangaroo(0, 3, 4, 2))
-	fmt.Println(kangaroo(0, 2, 5, 3))
-	fmt.Println(kangaroo(21, 6, 47, 3))
+	fmt.Println(breakingRecords([]int32{10, 5, 20, 20, 4, 5, 2, 25, 1}))
+	fmt.Println(breakingRecords([]int32{3, 4, 21, 36, 10, 28, 35, 5, 24, 42}))
 }
