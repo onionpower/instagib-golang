eb58404c1ede30a5ba25529e40369f5cf154b69d
onionpower <chocoonion@gmail.com>
2019-02-26 22:29:07 +0300

    Pow(x, n)

--- main.go
+++ main.go
@@ -5,29 +5,30 @@ import (
 	_ "github.com/segmentio/kafka-go/lz4"
 )
 
-func breakingRecords(scores []int32) []int32 {
-	if scores == nil || len(scores) == 0 {
-		return []int32{0, 0}
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
 	}
-	min, max := scores[0], scores[0]
-	var minScores, maxScores int32 = 0, 0
-	scoresLen := len(scores)
-	for i := 1; i < scoresLen; i++ {
-		it := scores[i]
-		if it < min {
-			min = it
-			minScores++
-			continue
-		}
-		if it > max {
-			max = it
-			maxScores++
-		}
+	if n < 0 {
+		n = -n
+		x = 1 / x
 	}
-	return []int32{maxScores, minScores}
+	return myPowInternal(x, n)
+}
+
+func myPowInternal(f float64, i int) float64 {
+	var res float64 = 1
+	for ; i > 0; i = i >> 1 {
+		if i&1 == 1 {
+			res *= f
+		}
+
+		f = f * f
+	}
+	return res
 }
 
 func main() {
-	fmt.Println(breakingRecords([]int32{10, 5, 20, 20, 4, 5, 2, 25, 1}))
-	fmt.Println(breakingRecords([]int32{3, 4, 21, 36, 10, 28, 35, 5, 24, 42}))
+	fmt.Println(myPow(4, 5))
+	fmt.Println(myPow(4, -2))
 }
