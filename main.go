073394f876e699f671b91c9728ebdf6560c86613
onionpower <chocoonion@gmail.com>
2019-03-26 19:59:50 +0300

    leetcode:  Available Captures for Rook, benchmark

--- main.go
+++ main.go
@@ -1,25 +1,96 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func lengthOfLongestSubstring(s string) int {
-	if len(s) < 2 {
-		return len(s)
-	}
+const Rook = 'R'
+const Pawn = 'p'
+const Empty = '.'
+const FieldLen = 8
 
-	return 0
+func numRookCaptures(board [][]byte) int {
+	x, y := findRook(board)
+	captures := 0
+	for h := x - 1; h >= 0; h-- {
+		if board[y][h] == Pawn {
+			captures++
+			break
+		}
+		if board[y][h] != Empty {
+			break
+		}
+	}
+	for h := x + 1; h < FieldLen; h++ {
+		if board[y][h] == Pawn {
+			captures++
+			break
+		}
+		if board[y][h] != Empty {
+			break
+		}
+	}
+	for h := y - 1; h >= 0; h-- {
+		if board[y][h] == Pawn {
+			captures++
+			break
+		}
+		if board[y][h] != Empty {
+			break
+		}
+	}
+	for h := y + 1; h < FieldLen; h++ {
+		if board[y][h] == Pawn {
+			captures++
+			break
+		}
+		if board[y][h] != Empty {
+			break
+		}
+	}
+	return captures
 }
 
-func maxLeft() int {
-	return 0
+func findRook(bytes [][]byte) (int, int) {
+	for x, row := range bytes {
+		for y, cell := range row {
+			if cell == Rook {
+				return y, x
+			}
+		}
+	}
+
+	panic(errors.New("it's a lie!"))
 }
 
 func main() {
-	printSln("asdf")
+	a := [][]string{
+		{".", ".", ".", ".", ".", ".", ".", "."},
+		{".", ".", ".", ".", ".", ".", ".", "."},
+		{".", ".", ".", ".", ".", ".", ".", "."},
+		{".", ".", ".", "R", ".", ".", ".", "."},
+		{".", ".", ".", ".", ".", ".", ".", "."},
+		{".", ".", ".", ".", ".", ".", ".", "."},
+		{".", ".", ".", ".", ".", ".", ".", "."},
+		{".", ".", ".", ".", ".", ".", ".", "."}}
+	printSln(a)
 }
 
-func printSln(s string) {
-	fmt.Println(lengthOfLongestSubstring(s))
+func toBytes(s [][]string) [][]byte {
+	res := make([][]byte, 0, 8)
+	for _, r := range s {
+		resRow := make([]byte, 0, 8)
+		for _, s := range r {
+			resRow = append(resRow, byte(s[0]))
+		}
+		res = append(res, resRow)
+	}
+	return res
+}
+
+func printSln(board [][]string) {
+	a := toBytes(board)
+	fmt.Println(board)
+	fmt.Printf("captures: %v\n", numRookCaptures(a))
 }
