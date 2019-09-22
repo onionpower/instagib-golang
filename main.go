3eabd4798a4c10373967db29ad6e16b267228c0b
onionpower <chocoonion@gmail.com>
2019-09-22 19:47:53 +0300

    boring fanin

--- main.go
+++ main.go
@@ -7,11 +7,10 @@ import (
 
 func main() {
 	fmt.Println(10e2)
-	ch := bc.BoringGen("me")
-	ach := bc.BoringGen("you")
+	ch := bc.FanIn(bc.BoringGen("me"), bc.BoringGen("you"))
+
 	for i := 0; i < 25; i++ {
 		fmt.Println(<-ch)
-		fmt.Println(<-ach)
 	}
 	fmt.Println("done")
 }
