2703bdb9a66d2371050d05fd8341578c5fd8f70d
onionpower <chocoonion@gmail.com>
2019-09-24 20:11:09 +0300

    boring select

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	bc "instagob/basicconcurrency"
+	"math/rand"
+	"time"
 )
 
 func main() {
@@ -9,6 +12,18 @@ func main() {
 	ch1 := bc.BoringGen("you", wait1)
 	wait2 := make(chan bool)
 	ch2 := bc.BoringGen("me", wait2)
-	ch := bc.FanIn(ch1, ch2)
-	bc.Listen(ch)
+	done := time.After(time.Duration(rand.Intn(2e3)+3e3) * time.Millisecond)
+	for {
+		select {
+		case v1 := <-ch1:
+			fmt.Println(v1)
+			wait1 <- true
+		case v2 := <-ch2:
+			fmt.Println(v2)
+			wait2 <- true
+		case <-done:
+			fmt.Println("done")
+			return
+		}
+	}
 }
