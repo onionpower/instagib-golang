5c70bfaaac1e6d6a2a0c90fd0612dcc0b7d3965b
onionpower <chocoonion@gmail.com>
2019-09-29 19:25:33 +0300

    string

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	bc "instagob/basicconcurrency"
+	"fmt"
+	s "instagob/structs"
+	"math/rand"
 )
 
 func main() {
-	bc.DaisyChan(50)
+	a := make([]int64, 0)
+	for i := 0; i < 30; i++ {
+		a = append(a, int64(i))
+	}
+	h := s.NewBheap(a...)
+	fmt.Println(h)
+
+	a = make([]int64, 0)
+	for i := 0; i < 30; i++ {
+		a = append(a, int64(rand.Intn(50)))
+	}
+	h = s.NewBheap(a...)
+	fmt.Println(h)
 }
