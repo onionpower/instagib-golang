019d21952ec0b1fdafcbd343b55f35485bb63fbb
onionpower <chocoonion@gmail.com>
2019-10-08 17:25:39 +0300

    boring ch merge

--- boringpipelines/pipelines.go
+++ boringpipelines/pipelines.go
@@ -1,5 +1,7 @@
 package boringpipelines
 
+import "sync"
+
 func Gen(n ...int) <-chan int {
 	out := make(chan int)
 	go func() {
@@ -17,6 +19,26 @@ func Sq(in <-chan int) <-chan int {
 		for i := range in {
 			out <- i * i
 		}
+		close(out)
+	}()
+	return out
+}
+
+func Merge(chs ...<-chan int) <-chan int {
+	out := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(len(chs))
+	fanin := func(in <-chan int) {
+		for v := range in {
+			out <- v
+		}
+		wg.Done()
+	}
+	for _, ch := range chs {
+		go fanin(ch)
+	}
+	go func() {
+		wg.Wait()
 		close(out)
 	}()
 	return out
--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
-	ch := bp.Sq(bp.Sq(bp.Gen(1, 2, 3)))
-	for v := range ch {
+	ch := bp.Gen(1, 2, 3, 5)
+	ch1 := bp.Sq(ch)
+	ch2 := bp.Sq(ch)
+	ch3 := bp.Sq(ch)
+	mch := bp.Merge(ch1, ch2, ch3)
+	for v := range mch {
 		fmt.Println(v)
 	}
 }
