ecf662b9b65d5b2be3a91ee379055c1d019ea62c
onionpower <chocoonion@gmail.com>
2019-11-03 12:32:23 +0300

    fib bot up

--- dp/fib.go
+++ dp/fib.go
@@ -10,3 +10,17 @@ func fibr(n int) int {
 
 	return fibr(n-1) + fibr(n-2)
 }
+
+func fibBotUp(n int) int {
+	if n <= 1 {
+		return 0
+	}
+
+	m := make([]int, n+1, n+1)
+	m[2] = 1
+	for i := 3; i <= n; i++ {
+		m[i] = m[i-1] + m[i-2]
+	}
+
+	return m[n]
+}
--- dp/fib_test.go
+++ dp/fib_test.go
@@ -5,6 +5,15 @@ import "testing"
 func TestFibRec(t *testing.T) {
 	//0 1 1 2 3 5 8
 	f := fibr(7)
+	assert(f, t)
+}
+
+func TestFibBotUp(t *testing.T) {
+	f := fibBotUp(7)
+	assert(f, t)
+}
+
+func assert(f int, t *testing.T) {
 	if f != 8 {
 		t.Errorf("expected is %v, but actual is %v", 8, f)
 	}
